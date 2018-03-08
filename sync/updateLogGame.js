084f2277a10d3f5a29919b5cebef3dfec9de634e
theuzer <jarvandemacio@gmail.com>
2018-03-08 10:23:54 +0000

    fix

--- sync/updateLogGame.js
+++ sync/updateLogGame.js
@@ -4,21 +4,21 @@ const logConnection = require('../database/azureDb').logConnection;
 const constants = require('../common/constants');
 
 exports.updateGameHasError = (id) => {
-  new sql.Request(logConnection).query(constants.updateGameHasError(id))
+  new sql.Request(logConnection).query(constants.queries.updateGameHasError(id))
     .catch((err) => {
       console.log(constants.errors.updateGameHasError, err);
     });
 };
 
 exports.updateGameIsProcessed = (id) => {
-  new sql.Request(logConnection).query(constants.updateGameIsProcessed(id))
+  new sql.Request(logConnection).query(constants.queries.updateGameIsProcessed(id))
     .catch((err) => {
       console.log(constants.errors.updateGameIsProcessed, err);
     });
 };
 
 exports.updateGameHasLeavers = (id) => {
-  new sql.Request(logConnection).query(constants.updateGameHasLeavers(id))
+  new sql.Request(logConnection).query(constants.queries.updateGameHasLeavers(id))
     .catch((err) => {
       console.log(constants.errors.updateGameHasLeavers, err);
     });
